5d3f773a1a90730afcdce10d5ee07df7e7c2cfa7
mdfayaz2 <kmdfayaz@lyrostech.com>
2024-09-25 14:47:11 +0530

    final

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/common/header/Header"
 import "./App.css"
 import Homepages from "./components/home/Homepages"
 import Footer from "./components/common/footer/Footer"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import SinglePage from "./components/singlePage/SinglePage"
 import Culture from "./components/culture/Culture"
 
@@ -12,11 +12,11 @@ const App = () => {
     <>
       <Router>
         <Header />
-        <Switch>
+        <Routes>
           <Route exact path='/' component={Homepages} />
           <Route path='/singlepage/:id' exact component={SinglePage} />
           <Route exact path='/culture' component={Culture} />
-        </Switch>
+        </Routes>
         <Footer />
       </Router>
     </>
